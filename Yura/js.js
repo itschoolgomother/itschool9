59e39f19fb05b64318503ee253ed1759f28152bc
itschoolgomother <itschoolgomother>
2020-11-25 13:23:16 +0200

    25.11.2020

---
+++ Yura/js.js
@@ -0,0 +1,13 @@
+let notaDo = new Audio();
+notaDo.src = "noty-do.mp3"
+
+document.getElementById('nDo')
+document.addEventListener("keydown",playPiano)
+
+function playPiano(e) {
+	switch(e.keyCode){
+		case 90:
+		notaDo.play();
+		break;	
+	}
+}
